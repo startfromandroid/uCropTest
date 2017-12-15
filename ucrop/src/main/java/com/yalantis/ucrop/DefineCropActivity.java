103745596ca683f410b2aa44ae1e37a90f267ec6
LAPTOP-49S9445Q\lishu <lishuai00410@outlook.com>
2017-12-15 19:05:20 +0800

    去掉toast

--- ucrop/src/main/java/com/yalantis/ucrop/DefineCropActivity.java
+++ ucrop/src/main/java/com/yalantis/ucrop/DefineCropActivity.java
@@ -60,6 +60,7 @@ public class DefineCropActivity extends AppCompatActivity implements View.OnClic
         } else if (i == R.id.state_done) {
             cropAndSaveImage();
         } else if (i == R.id.state_reset) {
+
             resetRotation();
         } else if (i == R.id.state_rotate) {
             rotateByAngle(90);
